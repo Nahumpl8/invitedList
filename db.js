28c2260e46befb19a19593ab92e8671f01decfdf
nahum_8 <nahumpl@MacBook-Pro-de-Nahum.local>
2023-11-19 23:11:43 +0100

    mejorando env

--- db.js
+++ db.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const mongoURI = process.env.MONGODB_URI;
 const secretKey = process.env.SECRET_KEY;
+const dataBase = process.env.DATA_BASE;
+const collection = process.env.COLLECTION;
 
 
 function conectar(){
@@ -14,7 +16,7 @@ function invitados(){
     return new Promise(async callback => {
         let conexion = await conectar();
 
-        let coleccion = conexion.db("weddin2024").collection("invitados");
+        let coleccion = conexion.db(dataBase).collection(collection);
 
         let invitados = await coleccion.find({}).toArray();
 
